56a80d63d490683b83d2c74698f0d2bde8dcdc84
Minskoleopc <minskoleopc@gmail.com>
2023-04-27 18:55:21 +0530

    added

---
+++ Oops/script.js
@@ -0,0 +1,94 @@
+// Object literal
+let chinmay = {
+    firstName:"chinmay",
+    lastName:"deshpande",
+    age:23,
+    rollNo:45
+}
+
+let amol = {
+    firstName:"amol",
+    lastName:"rao",
+    age:34,
+    rollNo:77
+}
+// retrive // update  //delete // add
+
+// function constructor 
+
+
+function Person(fn,ln,age,rn){
+    this.firstName = fn
+    this.lastName = ln 
+    this.age = age
+    this.rollNo = rn
+}
+
+let amolB = new Person("amolB","raoB",23,56)
+let amolC = new Person("amolC","raoC",27,55)
+let amolD = new Person("amolD","raoD",23,54)
+console.log(amolB)
+console.log(amolC)
+console.log(amolD)
+
+let vehicle = function(type,color){
+    this.type = type 
+    this.color = color
+}
+let audi  = new vehicle("SUV","red")
+let audi6  = new vehicle("SUV","green")
+
+
+// Es6 
+
+
+class PersonB {
+    constructor(fn,ln,age,roll){
+        this.firstName = fn
+        this.lastName = ln 
+        this.age = age
+        this.rollNo = roll
+    }
+
+}
+let amolG = new PersonB("amolg","raog",4,55)
+let amolI = new PersonB("amoli","raoe",4,66)
+console.log(amolG)
+console.log(amolI)
+
+
+let vehicleB = class {
+
+    constructor(type,color){
+        this.type  = type 
+        this.color = color
+    }
+
+}
+let k = new vehicleB("sedane","red")
+let l = new vehicleB("suv","blue")
+
+console.log(k)
+console.log(l)
+
+
+// Object.create 
+
+let k6 = Object.create({})
+
+console.log(k6)
+
+k6.firstName = "raj"
+k6.lastName = "sharma"
+k6.age = 34
+k6.rollNo = 98
+
+console.log(k6)
+
+
+
+
+
+
+
+
